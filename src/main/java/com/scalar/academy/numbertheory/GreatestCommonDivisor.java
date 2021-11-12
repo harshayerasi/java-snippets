15b71c26250aba457d31194085b7a90eda56bb7e
harshayerasi <harshayerasi@gmail.com>
2021-11-12 22:41:33 +0530

    Initial commit

---
+++ src/main/java/com/scalar/academy/numbertheory/GreatestCommonDivisor.java
@@ -0,0 +1,12 @@
+package com.scalar.academy.numbertheory;
+
+/**
+ * @author harsha
+ */
+public class GreatestCommonDivisor {
+
+    public static final int calculate(int[] array) {
+
+        return 0;
+    }
+}
